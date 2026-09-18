7c5c01dd0b7076a44492c9abe3d18f1539164691
Sardoro <sardor.jumaniyazov.90@mail.ru>
2026-09-18 23:38:08 +0200

    getting started with collections

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Form[] forms = {
-                new Kreis(5),
-                new Kreis(10),
+        List<Form> forms = new ArrayList<>();
 
-                new Quadrat(4),
-                new Quadrat(8),
-
-                new GIDreieck(3),
-                new GIDreieck(6)
-        };
+        forms.add(new Kreis(5));
+        forms.add(new Kreis(10));
+        forms.add(new Quadrat(4));
+        forms.add(new Quadrat(8));
+        forms.add(new GIDreieck(3));
+        forms.add(new GIDreieck(6));
 
         for (Form form : forms){
             form.info();
-
             System.out.println("Fläche: " + form.flaeche());
             System.out.println("Umfang: " + form.umfang());
-
             System.out.println();
         }
     }
