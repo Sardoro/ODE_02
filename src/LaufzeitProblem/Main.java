d5e0b6ebaaffcc485a7ff4fcbcb8c0f3b361a0c5
Sardoro <sardor.jumaniyazov.90@mail.ru>
2026-09-20 19:54:36 +0200

    Laufzeitproblem; NumberFormatException wurde mithilfe try-catch gelöst

--- src/LaufzeitProblem/Main.java
+++ src/LaufzeitProblem/Main.java
@@ -9,12 +9,21 @@ public class Main {
         numbers[0] = "10";
         numbers[1] = "20";
         numbers[2] = "30";
+
+        //Wir gehen die Basen von 10 bis 2 durch
         for (base = 10; base >= 2; --base) {
-            for (j = 0; j <= 3; ++j) {
-                i = Integer.parseInt(numbers[j],base);
-                System.out.println(numbers[j]+" base " + base + " = " +i);
+            //numbers.lenght ist 3, also nicht >=2
+            for (j = 0; j < numbers.length; ++j) {
+
+                try {
+                    //String wird mit der aktuellen Basis in eine normale Interger umgewandelt
+                    i = Integer.parseInt(numbers[j],base);
+                    System.out.println(numbers[j]+" base " + base + " = " +i);
+                } catch (NumberFormatException e) {
+                    //wenn die Zahl in dieser Basis nicht exestiert
+                    System.out.println(numbers[j] + " gibt es nicht in Basis " + base + "!");
+                }
             }
         }
-
     }
 }
